af397e1897bb12a981a6c65789a9ebe3b1f7dc17
Eran Raviv <eran@raviv360.com>
2026-04-23 20:06:13 +0300

    Fix env loading: use __dirname for dotenv, remove conflicting env_file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     name:          'waffle-walks',
     script:        'server.js',
     cwd:           '/home/ubuntu/waffle-walks/server',
-    env_file:      '/home/ubuntu/waffle-walks/server/.env',
     restart_delay: 5000,
     max_restarts:  10,
     watch:         false,
